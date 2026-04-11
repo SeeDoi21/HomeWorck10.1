98093d1b0aafc7860009e4f902bfa170ead774ef
Yurii Yan <yanyurii21@gmail.com>
2026-04-11 08:47:00 +0300

    add modified src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,6 @@
 # Импорт модуля datetime
 from datetime import datetime
-
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
@@ -9,9 +8,6 @@ def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List
     Фильтрует список словарей по ключу state
     """
     return [item for item in data if item.get('state') == state]
-
-
-
 
 
 def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
