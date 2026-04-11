1e5045c50ce7cb55e82fadc13b078cc14e5ee79b
Yurii Yan <yanyurii21@gmail.com>
2026-04-11 08:24:10 +0300

    add mosified src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,13 @@
+# Импорт модуля datetime
 from datetime import datetime
+
 from typing import List, Dict, Any
 
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
-
+    """
+    Фильтрует список словарей по ключу state
+    """
     return [item for item in data if item.get('state') == state]
 
 
@@ -11,7 +15,9 @@ def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List
 
 
 def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
-
+    """
+    Сортиует список словарей по date
+    """
     return sorted(
         data,
         key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
